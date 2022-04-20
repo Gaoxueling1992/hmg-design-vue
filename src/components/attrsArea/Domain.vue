27b05650a97259b3e4d558c095619a8a96e42311
sunny <gaoxl@hmgsoft.com>
2022-04-20 17:15:36 +0800

    业务规则增加维护功能

--- src/components/attrsArea/Domain.vue
+++ src/components/attrsArea/Domain.vue
@@ -34,18 +34,22 @@
     class="custom-class"
     title="业务规则配置"
     placement="right"
+    @close="editingRule=false"
   >
     <template v-if="activeCompObj.rules.length">
       <div class="title marginT10 marginB5 fontW500 fontS16">已有规则</div>
       <div v-for="(item, idx) in activeCompObj.rules" :key="idx">
-        <div class="fontW500">规则{{idx + 1}}</div>
+        <div class="fontW500">规则{{idx + 1}}
+          <i class="iconfont icondelete-border fr marginL10" @click="deleteIt(idx)"></i>
+          <i class="iconfont iconedit fr" @click="editIt(idx)"></i>
+        </div>
         {{opportunityMap[item.opportunity]}} 时, 当 {{+item.current===0?'当前控件':('业务控件 ' + item.threshold)}}
         {{ruleMap[item.elType]['conditionList'][item.type]}} {{item.value}} 时，
-        执行动作：{{actionList[item.id]}} {{item.content}}
+        执行动作-{{actionList[item.id]}}{{item.content ? (',内容：' + item.content) : ''}}{{item.label ? (',标签：' + item.label) : ''}}
       </div>
     </template>
     <template v-if="editingRule">
-      <div class="title marginT10 marginB5 fontW500 fontS16">新增规则(域值用${xx}代替)</div>
+      <div class="title marginT10 marginB5 fontW500 fontS16">{{editingIdx === -1 ? '新增' : '编辑'}}规则{{editingIdx === -1 ? '' : editingIdx+1}}(域值用${xx}代替)</div>
       <div class="title marginB5">执行时机</div>
       <a-select v-model:value="ruleObj.opportunity">
         <a-select-option v-for="(value, key) in opportunityMap" :key="key" :value="key">{{value}}</a-select-option>
@@ -200,18 +204,27 @@
         </a-select>
       </a-input-group>
       <a-input-group compact class="marginT5" v-if="ruleObj.id>4">
+        值
         <a-input v-model:value="ruleObj.content"></a-input>
+      </a-input-group>
+      <a-input-group compact class="marginT5" v-if="+ruleObj.id === 6">
+        标签
+        <a-input v-model:value="ruleObj.label"></a-input>
       </a-input-group>
       <a-button
         type="primary"
         class="marginT20"
         @click="saveRule"
       >保存规则</a-button>
+      <a-button
+        class="marginT20 marginL10"
+        @click="editingRule=false"
+      >取消</a-button>
     </template>
     <div
       v-else
       class="add-rule marginT20"
-      @click="editingRule=true"
+      @click="addRule"
     ><i class="iconfont iconplus"></i>添加规则</div>
   </a-drawer>
 </template>
@@ -235,9 +248,11 @@ export default defineComponent({
       current: 0,
       elType: 'text',
       id: '0',
-      content: ''
+      content: '',
+      label: ''
     });
     const editingRule: Ref<boolean> = ref<boolean>(false);
+    const editingIdx = ref<number>(-1);
 
     const addScript = () => {
       visibleDrawer.value = true;
@@ -257,7 +272,6 @@ export default defineComponent({
         });
         return;
       }
-      console.log(ruleObj.value.type, ruleObj.value.value, ruleObj.value.type < 6 && !ruleObj.value.value)
       switch (ruleObj.value.elType) {
         case 'text':
           if (ruleObj.value.type < 6 && !ruleObj.value.value) {
@@ -330,15 +344,15 @@ export default defineComponent({
         return;
       }
       editingRule.value = false;
-      activeCompObj.value.rules.push({
-        ...ruleObj.value,
-        condition: {
-          ...ruleObj.value.condition
-        },
-        action: {
-          ...ruleObj.value.action
-        }
-      });
+      if (editingIdx.value === -1) {
+        activeCompObj.value.rules.push({
+          ...ruleObj.value
+        });
+      } else {
+        activeCompObj.value.rules.splice(editingIdx.value, 1, {
+          ...ruleObj.value
+        })
+      }
       ruleObj.value= {
         opportunity: 'init',
         type: '0',
@@ -349,8 +363,39 @@ export default defineComponent({
         current: 0,
         elType: 'text',
         id: '0',
-        content: ''
+        content: '',
+        label: ''
       };
+    };
+
+    const deleteIt = (idx) => {
+      activeCompObj.value.rules.splice(idx, 1);
+    };
+
+    const editIt = (idx) => {
+      editingRule.value = true;
+      ruleObj.value = {
+        ...activeCompObj.value.rules[idx]
+      };
+      editingIdx.value = idx;
+    };
+     
+    const addRule = () => {
+      editingRule.value = true;
+      ruleObj.value = {
+        opportunity: 'init',
+        type: '0',
+        value: '',
+        min: 0,
+        max: 9999,
+        threshold: '',
+        current: 0,
+        elType: 'text',
+        id: '0',
+        content: '',
+        label: ''
+      };
+      editingIdx.value = -1
     };
 
     return {
@@ -363,7 +408,11 @@ export default defineComponent({
       editingRule,
       actionList,
       saveRule,
-      opportunityMap
+      opportunityMap,
+      deleteIt,
+      editingIdx,
+      editIt,
+      addRule
     };
   }
 });
