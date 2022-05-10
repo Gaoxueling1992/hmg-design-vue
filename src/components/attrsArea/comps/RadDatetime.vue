6801e48d11f23a310b2872fc29e1eac21b375440
sunny <gaoxl@hmgsoft.com>
2022-05-10 15:07:12 +0800

    PACS-8259

--- src/components/attrsArea/comps/RadDatetime.vue
+++ src/components/attrsArea/comps/RadDatetime.vue
@@ -1,25 +1,23 @@
 <template>
   <div class="title marginT10 marginB5 fontW500">选择器类型</div>
-  <div class="flex">
-    <a-select
-      class="flex-picker"
-      v-model:value="activeCompObj.picker"
-      :options="pickerList"
-      @change="changePicker"
-    />
-    <a-select
-      class="flex1 flex-format"
-      v-model:value="activeCompObj.format"
-      :options="pickerMap[activeCompObj.picker]"
-    />
-  </div>
+  <a-select
+    class="flex-picker"
+    v-model:value="activeCompObj.picker"
+    :options="pickerList"
+    @change="changePicker"
+  />
+  <a-select
+    class="flex-format marginT5"
+    v-model:value="activeCompObj.format"
+    :options="pickerMap[activeCompObj.picker]"
+  />
 </template>
 <script lang="ts">
 import { defineComponent, inject } from 'vue';
 import { pickerList, pickerMap } from '@/utils/config';
 
 export default defineComponent({
-  setup () {
+  setup() {
     const activeCompObj: any = inject('activeCompObj');
     const changePicker = (value: string) => {
       activeCompObj.value.format = pickerMap[value][0].value;
@@ -30,14 +28,12 @@ export default defineComponent({
       pickerMap,
       changePicker
     };
-  },
-})
+  }
+});
 </script>
 <style lang="scss" scoped>
+.flex-format,
 .flex-picker {
-  width: 71px;
-}
-.flex-format {
-  max-width: 165px;
+  width: 100%;
 }
 </style>
