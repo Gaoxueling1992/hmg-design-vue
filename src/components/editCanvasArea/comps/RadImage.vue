00d79edd166127cfae47fc83b4f0fb42b24efe0d
sunny <gaoxl@hmgsoft.com>
2022-05-23 16:01:27 +0800

    处理图片上传异常

--- src/components/editCanvasArea/comps/RadImage.vue
+++ src/components/editCanvasArea/comps/RadImage.vue
@@ -1,6 +1,7 @@
 <template>
   <a-image
     class="img"
+    v-if="ele.src"
     :height="ele.imgHeight"
     :width="ele.imgWidth"
     :src="ele.src"
@@ -13,14 +14,24 @@
       width: ele.imgWidth + 'px'
     }"
   />
+  <div
+    v-else
+    :style="{
+      marginTop: ele.styleSheet.paddingTop + 'px',
+      marginBottom: ele.styleSheet.paddingBottom + 'px',
+      marginLeft: ele.styleSheet.paddingLeft + 'px',
+      marginRight: ele.styleSheet.paddingRight + 'px',
+      height: ele.imgHeight+ 'px',
+      width: ele.imgWidth + 'px',
+      border: '1px solid'
+    }"
+  ></div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   props: ['ele'],
-  setup () {
-    
-  },
-})
+  setup() {}
+});
 </script>
