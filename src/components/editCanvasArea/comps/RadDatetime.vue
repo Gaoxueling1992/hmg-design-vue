05521f7e310e59a8ae6870f00f33f41e4860ee5b
sunny <gaoxl@hmgsoft.com>
2022-07-13 16:54:08 +0800

    fix: PACS-8915

--- src/components/editCanvasArea/comps/RadDatetime.vue
+++ src/components/editCanvasArea/comps/RadDatetime.vue
@@ -1,11 +1,3 @@
-<!--
- * @Author: sunny gaoxl@hmgsoft.com
- * @Date: 2022-01-24 11:00:44
- * @LastEditors: sunny gaoxl@hmgsoft.com
- * @LastEditTime: 2022-07-13 15:01:28
- * @FilePath: /hmg-design-vue/src/components/editCanvasArea/comps/RadDatetime.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <template>
   <div
     class="inherit"
@@ -24,6 +16,7 @@
     class="inherit"
     v-model:value="ele.value"
     placeholder="请选择时间"
+    :locale="locale"
     :picker="ele.picker"
     :format="ele.format"
   />
