a62dcd0e453d410eab14622e538bdf67228bf0c7
sunny <gaoxl@hmgsoft.com>
2022-07-21 10:55:39 +0800

    fix: PACS-8999

--- src/components/editCanvasArea/comps/RadDatetime.vue
+++ src/components/editCanvasArea/comps/RadDatetime.vue
@@ -2,7 +2,7 @@
  * @Author: sunny gaoxl@hmgsoft.com
  * @Date: 2022-01-24 11:00:44
  * @LastEditors: sunny gaoxl@hmgsoft.com
- * @LastEditTime: 2022-07-19 11:35:49
+ * @LastEditTime: 2022-07-21 10:52:40
  * @FilePath: /hmg-design-vue/src/components/editCanvasArea/comps/RadDatetime.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -27,6 +27,7 @@
     placeholder="请选择时间"
     :picker="ele.picker"
     :format="ele.format"
+    :locale="locale"
   />
   <div
     v-else
@@ -43,6 +44,7 @@
 <script lang="ts">
 import { defineComponent, inject, Ref } from 'vue';
 import moment from 'moment';
+import locale from 'ant-design-vue/es/date-picker/locale/zh_CN';
 
 export default defineComponent({
   props: ['ele', 'toolbarId', 'lineId'],
@@ -50,17 +52,24 @@ export default defineComponent({
     const isReadonlyStatus: Ref<boolean> = inject('isReadonlyStatus');
     return {
       isReadonlyStatus,
-      moment
+      moment,
+      locale
     };
   }
 });
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 .ele-label {
   padding-right: 10px;
   display: inline-block;
 }
 .display-text {
   display: inline-block;
+}
+.inherit {
+  border-color: inherit;
+  color: inherit !important;
+  background-color: inherit;
+  font-size: inherit;
 }
 </style>
