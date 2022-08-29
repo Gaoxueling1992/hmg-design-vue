fd3e39d2abef3db2e8974e820759aba5bb4360d9
sunny <gaoxl@hmgsoft.com>
2022-08-29 17:26:48 +0800

    fix: reset theme

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: sunny gaoxl@hmgsoft.com
  * @Date: 2022-03-02 18:08:41
  * @LastEditors: sunny gaoxl@hmgsoft.com
- * @LastEditTime: 2022-08-29 15:49:08
+ * @LastEditTime: 2022-08-29 17:26:36
  * @FilePath: /hmg-design-vue/src/main.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -15,7 +15,7 @@ import store from './store'
 import 'ant-design-vue/dist/antd.less'
 import './assets/iconfont/iconfont.scss'
 
-initTheme('green')
+initTheme('default')
 
 const app = createApp(App)
 app.use(router)
